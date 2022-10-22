d244f4f2f0ae97c240a44b2904beb75eab1406b2
oleg <gelonooz@gmail.com>
2022-10-22 08:36:24 +0300

    update elements_test added tests: test_web_table test_web_table_search_person

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -30,3 +30,21 @@ class RadioButtonLocators():
     RADIO_BUTTON_IMPRESSIVE = (By.CSS_SELECTOR, "label[for='impressiveRadio']")
     RADIO_BUTTON_NO = (By.CSS_SELECTOR, "label[for='noRadio']")
     SELECTED_RESULT = (By.CSS_SELECTOR, "span[class='text-success']")
+
+
+class WebTablesLocators():
+    # add person form
+    ADD_BUTTON = (By.CSS_SELECTOR, "button[id='addNewRecordButton']")
+    FIRST_NAME_INPUT = (By.CSS_SELECTOR, "input[id='firstName']")
+    LAST_NAME_INPUT = (By.CSS_SELECTOR, "input[id='lastName']")
+    EMAIL_INPUT = (By.CSS_SELECTOR, "input[id='userEmail']")
+    AGE_INPUT = (By.CSS_SELECTOR, "input[id='age']")
+    SALARY_INPUT = (By.CSS_SELECTOR, "input[id='salary']")
+    DEPARTMENT_INPUT = (By.CSS_SELECTOR, "input[id='department']")
+    SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[id='submit']")
+
+    # table
+    FULL_PEOPLE_LIST = (By.CSS_SELECTOR, "div[class='rt-tr-group']")
+    SEARCH_INPUT = (By.CSS_SELECTOR, "input[id='searchBox']")
+    DELETE_BUTTON = (By.CSS_SELECTOR, "span[title='Delete']")
+    ROW_PARENT = ".//ancestor::div[@class='rt-tr-group']"
