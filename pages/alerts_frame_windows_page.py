cef9bc32fe3cedd3e5bb2708e2dfaeb68de3a888
gelono <gelonooz@gmail.com>
2022-11-03 10:20:39 +0200

    added class:  TestAlertsPage added tests:  - test_see_alert  - test_alert_appear_5_sec  - test_confirm_alert  - test_prompt_alert

--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -1,4 +1,9 @@
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+import random
+import time
+
+from selenium.common import UnexpectedAlertPresentException
+
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.basepage import BasePage
 
 
@@ -16,3 +21,36 @@ class BrowserWindowsPage(BasePage):
         text_title = self.is_present(self.locators.TITLE_NEW).text
         return text_title
 
+class AlertsPage(BasePage):
+    locators = AlertsPageLocators()
+
+    def see_alert(self):
+        self.is_visible(self.locators.SEE_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.is_visible(self.locators.APPEAR_ALERT_5_SEC_BUTTON).click()
+        time.sleep(6)
+        try:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+        except UnexpectedAlertPresentException:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+
+    def check_confirm_alert(self):
+        self.is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.accept()
+        text_result = self.is_present(self.locators.CONFIRM_RESULT).text
+        return text_result
+
+    def check_prompt_alert(self):
+        text = f'autotest{random.randint(0,100)}'
+        self.is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.is_present(self.locators.PROPMT_RESULT).text
+        return text, text_result
--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
@@ -19,3 +19,30 @@ class TestAlertsFrameWindow:
             new_tab_window.open()
             text_result = new_tab_window.check_opened_new_window()
             assert text_result == 'This is a sample page', 'The new window has not opened or an incorrect window has opened'
+
+
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.see_alert()
+            assert alert_text == 'You clicked a button', 'Alert did not show up'
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', 'Alert did not show up'
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', 'Alert did not show up'
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert alert_text == f'You entered {text}', 'Alert did not show up'
