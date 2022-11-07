af6d151499c5f100ce8a1bc0d73020ac88536d86
gelono <gelonooz@gmail.com>
2022-11-07 11:25:23 +0200

    added negative message in TestAccordianPage added TestAutoCompletePage added generated_color, dataclass Colors

--- pages/widgets_page.py
+++ pages/widgets_page.py
@@ -1,6 +1,10 @@
-from selenium.common import TimeoutException
+import random
 
-from locators.widgets_page_locators import AccordianPageLocators
+from selenium.common import TimeoutException
+from selenium.webdriver import Keys
+
+from generator.generator import generated_color
+from locators.widgets_page_locators import AccordianPageLocators, AutoCompletePageLocators
 from pages.basepage import BasePage
 
 
@@ -26,3 +30,42 @@ class AccordianPage(BasePage):
             section_title.click()
             section_content = self.is_visible(accordian[accordian_num]['content']).text
         return [section_title.text, len(section_content)]
+
+class AutoCompletePage(BasePage):
+    locators = AutoCompletePageLocators()
+
+    def fill_input_multi(self):
+        colors = random.sample(next(generated_color()).color_name, k=random.randint(2, 4))
+        for color in colors:
+            input_multi = self.is_clickable(self.locators.MULTI_INPUT)
+            input_multi.click()
+            input_multi.send_keys(color)
+            input_multi.send_keys(Keys.ENTER)
+        return colors
+
+    def remove_value_from_multi(self):
+        count_value_before = len(self.are_present(self.locators.MULTI_VALUE))
+        remove_button_list = self.are_visible(self.locators.MULTI_VALUE_REMOVE)
+        for value in remove_button_list:
+            value.click()
+            break
+        count_value_after = len(self.are_present(self.locators.MULTI_VALUE))
+        return count_value_before, count_value_after
+
+    def check_color_in_multi(self):
+        color_list = self.are_present(self.locators.MULTI_VALUE)
+        colors = []
+        for color in color_list:
+            colors.append(color.text)
+        return colors
+
+    def fill_input_single(self):
+        color = random.sample(next(generated_color()).color_name, k=1)
+        input_single = self.is_clickable(self.locators.SINGLE_INPUT)
+        input_single.send_keys(color)
+        input_single.send_keys(Keys.ENTER)
+        return color[0]
+
+    def check_color_in_single(self):
+        color = self.is_visible(self.locators.SINGLE_VALUE)
+        return color.text
