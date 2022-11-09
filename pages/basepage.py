f03e7eb875e1b2f3384e36f8846aaad04e7cc06d
gelono <gelonooz@gmail.com>
2022-11-09 12:29:15 +0200

    created class Interactions created test Sortable

--- pages/basepage.py
+++ pages/basepage.py
@@ -58,3 +58,8 @@ class BasePage:
         action = ActionChains(self.driver)
         action.drag_and_drop_by_offset(element, x_coords, y_coords)
         action.perform()
+
+    def action_drag_and_drop_to_element(self, what, where):
+        action = ActionChains(self.driver)
+        action.drag_and_drop(what, where)
+        action.perform()
